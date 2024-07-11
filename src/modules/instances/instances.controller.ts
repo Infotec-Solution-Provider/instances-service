c080afdaa0d5debd2c7d0d2093c26ae3ec8695a6
Renan G Dutra <r.granatodutra@gmail.com>
2024-07-11 04:50:55 -0300

    feat: add migrate schedule routine (temp)

--- src/modules/instances/instances.controller.ts
+++ src/modules/instances/instances.controller.ts
@@ -5,6 +5,9 @@ import { CreateInstanceDto } from "./dto/create-instance.dto";
 import InstancesService from "./instances.service";
 import { NotFoundError } from "@rgranatodutra/http-errors";
 import AuthService from "../auth/auth.service";
+import PoolsService from "../pools/pools.service";
+import { RowDataPacket } from "mysql2";
+import axios from "axios";
 
 class InstancesController {
     public readonly router: core.Router;
@@ -15,6 +18,42 @@ class InstancesController {
         this.router.post("/api/instances", AuthService.validateTokenMiddleware, validateDto(CreateInstanceDto), this.create);
         this.router.get("/api/instances", AuthService.validateTokenMiddleware, this.list);
         this.router.get("/api/instances/:clientName", AuthService.validateTokenMiddleware, this.getOneByName);
+        this.router.get("/api/instances/schedules-routine", AuthService.validateTokenMiddleware, this.routine)
+    }
+
+    private async routine(req: Request, res: Response): Promise<Response> {
+        const allInstances = await InstancesService.list();
+        const generatedSchedules: any[] = [];
+
+        for (const instance of allInstances) {
+            const oldSchedules = await PoolsService.execute(
+                instance.name,
+                "SELECT * FROM w_atendimentos WHERE DATA_AGENDAMENTO IS NOT NULL AND AGUARDANDO_RETORNO = 'SIM'",
+                []
+            );
+
+            if (!Array.isArray(oldSchedules)) {
+                continue;
+            }
+
+            for (const oldSchedule of oldSchedules as unknown as RowDataPacket[]) {
+                const originalTimestamp = new Date(oldSchedule.DATA_AGENDAMENTO).getTime();
+                const ajustedTimestamp = originalTimestamp - (3 * 60 * 60 * 1000);
+                const ajustedDate = new Date(ajustedTimestamp);
+
+                await axios.post(`http://localhost:7002/api/wa-schedules/${instance.name}`, {
+                    scheduleDate: ajustedDate,
+                    whatsappId: oldSchedule.CODIGO_NUMERO,
+                    toUserId: oldSchedule.CODIGO_OPERADOR,
+                    byUserId: oldSchedule.CODIGO_OPERADOR,
+                    sectorId: oldSchedule.SETOR
+                })
+                    .then((res) => generatedSchedules.push(res.data.data))
+                    .catch((err) => console.log(err?.response?.data?.message || err));
+            }
+        }
+        return res.status(201).json({ message: "successful migrated schedules", data: generatedSchedules })
+
     }
 
     private async create(req: Request, res: Response): Promise<Response> {
