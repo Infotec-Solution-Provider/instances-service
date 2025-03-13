ffb4b67c86eb6517537a24210588d2f14213c27e
Renan G Dutra <r.granatodutra@gmail.com>
2025-03-13 13:12:03 -0300

    refactor: add utility dependency and improve error logging in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,24 @@
 import "dotenv/config";
 import "express-async-errors";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import { handleRequestError } from "@rgranatodutra/http-errors";
-
 import InstancesController from "./controllers/instances.controller";
-
-import getRouterEndpoints from "inpulse-crm/utils/src/getRouterEndpoints.util";
 import ServersController from "./controllers/servers.controller";
 import ParametersController from "./controllers/parameters.controller";
 import PoolsController from "./controllers/pools.controller";
 import AuthController from "./controllers/auth.controller";
+import { Logger, logRoutes } from "@in.pulse-crm/utils";
 
 const app = express();
 const appPort = Number(process.env["LISTEN_PORT"]) || 8000;
 
 const controllers = {
-  instances: new InstancesController(),
-  servers: new ServersController(),
-  parameters: new ParametersController(),
-  pools: new PoolsController(),
-  auth: new AuthController(),
+	instances: new InstancesController(),
+	servers: new ServersController(),
+	parameters: new ParametersController(),
+	pools: new PoolsController(),
+	auth: new AuthController(),
 };
 
 app.use(express.json());
@@ -31,9 +29,14 @@ app.use(controllers.servers.router);
 app.use(controllers.parameters.router);
 app.use(controllers.pools.router);
 
-Object.values(controllers).forEach((c) => {
-  const e = getRouterEndpoints(c.router, "");
-  e.forEach((r) => console.log(`[ROUTE] ${r}`));
+logRoutes(
+	"",
+	Object.values(controllers).map((c) => c.router),
+);
+
+app.use((err: Error, req: Request, _res: Response, next: NextFunction) => {
+	Logger.error(req.url, err);
+	next(err);
 });
 
 app.use(handleRequestError);
